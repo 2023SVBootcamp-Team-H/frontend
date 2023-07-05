838057b78d81ffeec86efa826c4f8747cff6e87e
“mineii” <kmaoo0406@naver.com>
2023-07-06 05:11:48 +0900

    feat: 프로젝트 초기 세팅

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
