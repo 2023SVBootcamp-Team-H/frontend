907ae512c5af07d39d229357c9c6d05195bd05af
“mineii” <kmaoo0406@naver.com>
2023-07-11 01:24:46 +0900

    design: Animation css 적용

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import 'animate.css';
+// import family from '@/assets/images/category/family.png';
 import frame from '@/assets/images/frame.png';
 
 function Book() {
   return (
-    <div
-      className="bg-pageBackgroud outline outline-[15px] rounded-md outline-pageOutline w-fit h-fit
-    flex "
-    >
-      <img className="p-3" src={frame} alt="frame" />
-      <div className="border-solid border-x-2 h-auto border-[#A9A9A9] blur-sm" />
-      <img className="p-3" src={frame} alt="frame" />
-      <div>이미지</div>
+    <div>
+      <div
+        className="bg-pageBackgroud outline outline-[15px] rounded-md outline-pageOutline w-fit h-fit
+    flex 
+    animate__animated animate__jackInTheBox"
+      >
+        <img className="p-3" src={frame} alt="frame" />
+        <div className="border-solid border-x-2 h-auto border-[#A9A9A9] blur-sm" />
+        <img className="p-3" src={frame} alt="frame" />
+      </div>
+
+      {/* <div>
+        <img className="" src={family} alt="family" />
+      </div> */}
     </div>
   );
 }
