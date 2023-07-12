4d8b0158f238b1bb442369923ac28757a91daccb
“mineii” <kmaoo0406@naver.com>
2023-07-13 02:59:54 +0900

    design: 구분선 추가

--- src/components/StatisticsL.tsx
+++ src/components/StatisticsL.tsx
@@ -6,18 +6,21 @@ import youngboy from '@/assets/images/category/youngboy.png';
 function StatisticsL() {
   return (
     <div className="flex flex-col justify-center  items-center font-ham-m  text-textTitle ">
-      <div className="flex flex-col  justify-center items-center mt-10 space-y-4">
+      <div className="flex flex-col justify-center items-center space-y-4  mt-9">
         <span className=" text-[25px]">인기도 1위 인격</span>
         <img className="w-48 " src={grandma} alt="grandma" />
         <span className="text-[22px]">할머니</span>
       </div>
 
-      <div className="flex mt-16">
+      <div className="border border-solid border-[#DBD4C7] w-96 mt-10" />
+
+      <div className="flex mt-8 space-x-2">
         <div className="flex flex-col justify-center items-center space-y-4">
           <span className=" text-[15px]">여성이 뽑은 1위</span>
           <img className="w-48 " src={parents} alt="parents" />
           <span className="text-[12px]">부모님</span>
         </div>
+        <div className="border-solid border h-auto  border-[#DBD4C7] " />
         <div className="flex flex-col justify-center items-center space-y-4">
           <span className=" text-[15px]">남성이 뽑은 1위</span>
           <img className="w-40 " src={youngboy} alt="youngboy" />
