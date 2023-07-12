ca3069a8a6c9c6444c80408f2be5a75adf51eef2
“mineii” <kmaoo0406@naver.com>
2023-07-13 03:01:41 +0900

    design: 세부 디자인 소폭 수정

--- src/components/StatisticsL.tsx
+++ src/components/StatisticsL.tsx
@@ -6,10 +6,10 @@ import youngboy from '@/assets/images/category/youngboy.png';
 function StatisticsL() {
   return (
     <div className="flex flex-col justify-center  items-center font-ham-m  text-textTitle ">
-      <div className="flex flex-col justify-center items-center space-y-4  mt-9">
-        <span className=" text-[25px]">인기도 1위 인격</span>
-        <img className="w-48 " src={grandma} alt="grandma" />
-        <span className="text-[22px]">할머니</span>
+      <div className="flex flex-col justify-center items-center space-y-4  mt-12">
+        <span className=" text-[23px]">인기도 1위 인격</span>
+        <img className="w-44 " src={grandma} alt="grandma" />
+        <span className="text-[20px]">할머니</span>
       </div>
 
       <div className="border border-solid border-[#DBD4C7] w-96 mt-10" />
