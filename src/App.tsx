b563610089382073a9f0d9302ce6c17dd1a862c8
“mineii” <kmaoo0406@naver.com>
2023-07-13 14:50:43 +0900

    chore: highchart 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BookPage from '@/pages/BookPage';
-// import BookPageTwo from '@/pages/BookPageTwo';
+import BookPageTwo from '@/pages/BookPageTwo';
 import MainPage from '@/pages/MainPage';
 import BookPageFour from './pages/BookPageFour';
 
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/book" element={<BookPage />} />
-        {/* <Route path="/book2" element={<BookPageTwo />} /> */}
+        <Route path="/book2" element={<BookPageTwo />} />
         <Route path="/book4" element={<BookPageFour />} />
       </Routes>
     </BrowserRouter>
