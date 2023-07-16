b59fd791f618887cc2bb7a8ac5cf31ee997d1fa9
“mineii” <kmaoo0406@naver.com>
2023-07-17 01:56:18 +0900

    comment: 푸시 확인용

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -8,6 +8,7 @@ function Book() {
       className="bg-pageBackgroud outline outline-[15px] rounded-md outline-pageOutline w-fit h-fit
     flex "
     >
+      {/* 푸시 확인용 주석 */}
       <img className="p-3" src={frame} alt="frame" />
       <div className="border-solid border-x-2 h-auto border-[#A9A9A9] blur-sm" />
       <img className="p-3" src={frame} alt="frame" />
