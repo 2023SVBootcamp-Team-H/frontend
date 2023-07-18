b02848826f526092f06373cded11b54c35c12de6
sori830 <solhwi830@gmail.com>
2023-07-18 13:56:17 +0900

    comment: 필요한 주석 추가 및 변경

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import 'animate.css';
 import { useNavigate } from 'react-router-dom';
 import SelectWorryL from '@/components/SelectWorryL';
