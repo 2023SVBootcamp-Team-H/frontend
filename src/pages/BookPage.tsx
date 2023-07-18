6fd0975997c5f99f588fe29659bf28b4159e5135
sori830 <solhwi830@gmail.com>
2023-07-18 13:54:47 +0900

    feat: 고민 카테고리 버튼 클릭 시 색상 변경

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'animate.css';
+import { useNavigate } from 'react-router-dom';
 import SelectWorryL from '@/components/SelectWorryL';
 import SelectWorryR from '@/components/SelectWorryR';
 // import HTMLFlipBook from 'react-pageflip';
@@ -7,6 +8,13 @@ import SelectWorryR from '@/components/SelectWorryR';
 // import frame from '@/assets/images/frame.png';
 
 function BookPage() {
+  const navigate = useNavigate(); // react-router-dom useNavigate 사용 선언
+
+  function toBookPageTwo() {
+    // react-router-dom을 이용한 고민 입력 페이지로 이동 함수
+    navigate('/book2');
+  }
+
   return (
     // 배경
     <div
@@ -17,15 +25,15 @@ function BookPage() {
       <div
         className=" animate__animated animate__jackInTheBox
         flex justify-center items-center
-        w-[fit] h-[fit] p-4 bg-pageBackgroud 
-       outline-pageOutline outline outline-[15px] rounded-md 
+        w-[fit] h-[fit] p-4 bg-pageBackgroud
+       outline-pageOutline outline outline-[15px] rounded-md
        px-4 pt-5 pb-8"
       >
         {/* 왼쪽 페이지 */}
         <div
           className="bg-pageBackgroud  h-[620px] w-[450px]
-        bg-bookframe bg-center bg-origin-padding p-3 bg-contain bg-no-repeat 
-        border-solid border-r-[3px] border-[#D9D3C8] 
+        bg-bookframe bg-center bg-origin-padding p-3 bg-contain bg-no-repeat
+        border-solid border-r-[3px] border-[#D9D3C8]
         flex flex-col items-center
         "
         >
@@ -35,12 +43,13 @@ function BookPage() {
         {/* 오른쪽 페이지 */}
         <div
           className="bg-pageBackgroud h-[620px] w-[450px]
-      bg-bookframe bg-center bg-origin-padding p-3 bg-contain bg-no-repeat 
+      bg-bookframe bg-center bg-origin-padding p-3 bg-contain bg-no-repeat
   flex flex-col items-center"
         >
           {/* 오른쪽 이미지 */}
           <SelectWorryR />
           <button
+            onClick={() => toBookPageTwo()}
             type="button"
             className="px-6 py-1 mt-10
         bg-[#ECE6F3] rounded-full border border-solid border-[#7C5197]
@@ -55,3 +64,55 @@ function BookPage() {
 }
 
 export default BookPage;
+
+// //////////////////////////////////
+
+// interface SelectWorryProps {
+//   selectedButton: string;
+//   handleSelectButton: (category: string) => void;
+// }
+
+// function BookPage() {
+//   const navigate = useNavigate();
+//   const [selectedButton, setSelectedButton] = useState<string>('');
+
+//   const handleSelectButtonL = (category: string) => {
+//     setSelectedButton(category);
+//   };
+
+//   const handleSelectButtonR = (category: string) => {
+//     setSelectedButton(category);
+//   };
+
+//   function toBookPageTwo() {
+//     navigate('/book2');
+//   }
+
+//   return (
+//     <div className="bg-dontworrybg bg-bgmain min-h-screen w-full bg-contain bg-no-repeat bg-center flex justify-center items-center">
+//       <div className="animate__animated animate__jackInTheBox flex justify-center items-center w-[fit] h-[fit] p-4 bg-pageBackgroud outline-pageOutline outline outline-[15px] rounded-md px-4 pt-5 pb-8">
+//         <div className="bg-pageBackgroud h-[620px] w-[450px] bg-bookframe bg-center bg-origin-padding p-3 bg-contain bg-no-repeat border-solid border-r-[3px] border-[#D9D3C8] flex flex-col items-center">
+//           <SelectWorryL
+//             selectedButton={selectedButton}
+//             handleSelectButton={handleSelectButtonL}
+//           />
+//         </div>
+//         <div className="bg-pageBackgroud h-[620px] w-[450px] bg-bookframe bg-center bg-origin-padding p-3 bg-contain bg-no-repeat flex flex-col items-center">
+//           <SelectWorryR
+//             selectedButton={selectedButton}
+//             handleSelectButton={handleSelectButtonR}
+//           />
+//           <button
+//             onClick={() => toBookPageTwo()}
+//             type="button"
+//             className="px-6 py-1 mt-10 bg-[#ECE6F3] rounded-full border border-solid border-[#7C5197] text-[#7C5197] font-ham-l text-center"
+//           >
+//             Next
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default BookPage;
