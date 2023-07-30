189f0f2c8502a5b8fc3b2ee9ecb22881ad3d3081
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-07-31 01:09:14 +0900

    fix : img 세로 정렬

--- src/components/NickameL.tsx
+++ src/components/NickameL.tsx
@@ -5,7 +5,9 @@ import nicknameLottie from '@/assets/nicknameLottie';
 
 function NickameL() {
   return (
-    <div className={`${'w-[100%]'} h-[100%] object-cover`}>
+    <div
+      className={`${'w-[100%]'} h-[100%] flex flex-col justify-center object-cover`}
+    >
       {/* <Lottie animationData={nicknameLottie} data-testid="lottie-animation" /> */}
       <img className="" src={nicknameL} alt="witch" />
     </div>
