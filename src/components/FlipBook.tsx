aafa9e739b2cc86399befafb980e6a19e269ca87
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-08-02 18:25:48 +0900

    fix: 음원 파일 오류 수정

--- src/components/FlipBook.tsx
+++ src/components/FlipBook.tsx
@@ -31,6 +31,8 @@ import {
   widthPerHeight2,
 } from '@/assets/values';
 import 'animate.css';
+import bookSound from '../assets/audio/book-in.mp3';
+import booksori from '../assets/audio/booksori-sarasvatl.mp3';
 
 function FlipBook() {
   const [isOpenModal, setOpenModal] = useState<boolean>(() => false);
@@ -41,7 +43,7 @@ function FlipBook() {
   const flipPageRef = useRef<ReactFlipPage | null>(null);
 
   const flipSound = () => {
-    const audio = new Audio('./src/assets/audio/booksori-sarasvatl.mp3');
+    const audio = new Audio(booksori);
     audio.volume = volume;
     if (isPlaying) {
       audio.play();
@@ -59,7 +61,7 @@ function FlipBook() {
   let isBookin = false;
   useEffect(() => {
     if (!isBookin) {
-      const audio = new Audio('./src/assets/audio/book-in.mp3');
+      const audio = new Audio(bookSound);
       audio.volume = volume;
       if (isPlaying) {
         audio.play();
